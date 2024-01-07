58d9c9958fe1138bd35044d4d6dcb9588c59451b
eggmanQQQ <exzero@126.com>
2024-01-07 10:48:29 +0800

    更新元组的内容

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use ferris_says::{self, say};
 use std::io::{BufWriter, stdout};
 
+///
+/// 参考内容
+/// https://doc.rust-lang.org/rust-by-example
+/// https://rusty.course.rs/
+/// 
+
 // 引用其他文件的方式
 mod structures;
 use crate::structures::{Point, Rectangle, Unit, Pair};
 
+const _MAX_POINT: u32 = 100_000;
+
 fn main() {
     the_first_case();
 
-    // function_reference();
-    // learn_promitives();
+    function_reference();
+    learn_promitives();
 
     learn_structures();
 }
@@ -34,11 +42,15 @@ fn function_reference() {
     println!("s2: {}", s2);
 }
 
+struct Struct {
+    e: i32
+}
 // Promitives 基本数据结构
 fn learn_promitives() {
-    // 
-    let logical: bool = true;
-    println!("{}", logical);
+    // 变量绑定概念 - logical 拥有 ‘true’ 内存的所有权
+    // 没有 var 的概念，所有变量默认不可变，需要通过 mut 修饰变量使其可变
+    let _logical: bool = true;
+    // println!("{}", logical);
 
     // 支持前后标注变量类型
     let a_float: f64 = 1.0;
@@ -64,8 +76,20 @@ fn learn_promitives() {
 
     // mutable = true; // 会报错，变量的类型不能改变
     let mutable = true; // 变量可以通过重复声明（重写）来更新类型和值
-
     println!("{}", mutable);
+
+    // 对复杂变量进行解构
+    let (a, mut b): (bool, bool) = (true, false);
+    println!("a = {:?}, b = {:?}", a, b);
+    b = true;
+    assert_eq!(a, b);
+
+    // 进行更复杂的结构赋值
+    let (a,b,c,d,e);
+    (a, b) = (1, 2); // 元组
+    [c, .., d, _] = [1, 2, 3, 4, 6]; // 切片
+    Struct {e, ..} = Struct {e: 5}; // 结构题
+    assert_eq!([1,2,3,6,0], [a,b,c,d,e]);
 }
 
 fn learn_structures() {
